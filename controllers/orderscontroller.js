3153529c9b9187db0e946d5cf4b5f40b48738161
cewerling <cwerling@cornerstones.com>
2020-04-29 22:26:29 -0400

    end of day

--- controllers/orderscontroller.js
+++ controllers/orderscontroller.js
@@ -7,6 +7,8 @@ const validateSession = require('../middleware/validate-session');
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 
+
+
 /**************************************************
 * GET All Orders for the Signed In User/Salesperson
 ***************************************************/
@@ -27,6 +29,82 @@ router.get('/', validateSession, (req, res) => {
 
 
 
+
+/****************************************
+* CREATE Order
+****************************************/
+
+router.post('/', validateSession, (req, res) => {
+
+    const orderFromRequest = {
+        "customerName": req.body.order.customerName,
+        "orderNumber": req.body.order.orderNumber,
+        "orderPlacedDateTime": req.body.order.orderPlacedDateTime,
+        "mobilePhone": req.body.order.mobilePhone,
+        "email": req.body.order.email,
+        "hostName": req.body.order.hostName,
+        "address1": req.body.order.address1,
+        "city": req.body.order.city,
+        "region": req.body.order.region,
+        "postalCode": req.body.order.postalCode,
+        "orderStatus": req.body.order.orderStatus,
+        "costSubtotal": req.body.order.costSubtotal,
+        "userid": req.user.salesUserID
+    }
+
+    console.log('******** Order to Create: ********');
+    console.dir(orderFromRequest);
+
+    OrderHeader.create(orderFromRequest)
+      .then(log => {res.status(200).json(log);})
+      .catch(err => res.json(req.errors))
+
+});
+
+
+
+
+/****************************************
+* UPDATE Order
+****************************************/
+
+router.put('/:id', validateSession, (req, res) => {
+
+    const orderFromRequest = {
+        "customerName": req.body.order.customerName,
+        "orderNumber": req.body.order.orderNumber,
+        "orderPlacedDateTime": req.body.order.orderPlacedDateTime,
+        "mobilePhone": req.body.order.mobilePhone,
+        "email": req.body.order.email,
+        "hostName": req.body.order.hostName,
+        "address1": req.body.order.address1,
+        "city": req.body.order.city,
+        "region": req.body.order.region,
+        "postalCode": req.body.order.postalCode,
+        "orderStatus": req.body.order.orderStatus,
+        "costSubtotal": req.body.order.costSubtotal,
+    }
+
+    OrderHeader.update(orderFromRequest, {where: {userId: req.user.salesUserID, id: req.params.id}})
+      .then(log => {res.status(200).json(log);})
+      .catch(err => res.json(req.errors))
+
+});
+
+
+
+
+/****************************************
+* Delete Individual Orders
+****************************************/
+
+router.delete('/:id', validateSession, (req, res) => {
+
+    OrderHeader.destroy({where: {userId: req.user.salesUserID, id: req.params.id}})
+      .then(log => {res.status(200).json(log);})
+      .catch(err => res.json(req.errors))
+
+});
 
 
 
